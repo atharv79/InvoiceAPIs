17d1356424d6f348bcee8aed7f26b5ab87a865a9
Atharv Pawar <pawaratharv79@gmail.com>
2026-02-24 15:29:32 +0530

    Submission
    Folders

---
+++ API/Controllers/InvoiceViewController.cs
@@ -0,0 +1,28 @@
+﻿using InvoiceAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceAPI.Controllers
+{
+    public class InvoiceViewController : Controller
+    {
+        private readonly InvoiceService _invoiceService;
+
+        public InvoiceViewController(InvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
+        //URL: /invoice/view/1
+        public IActionResult View( int id)
+        {
+            var invoice = _invoiceService.GetInvoicebyID(id);
+
+            if(invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+    }
+}
---
+++ API/Services/InvoiceService.cs
@@ -0,0 +1,70 @@
+﻿using InvoiceAPI.Models;
+
+namespace InvoiceAPI.Services
+{
+    public class InvoiceService
+    {
+
+        private readonly List<Invoice> _invoices;
+
+        public InvoiceService()
+        {
+            _invoices = new List<Invoice>
+            {
+                new Invoice
+                {
+                    Id = 1,
+                InvoiceNumber = "INV - 0001",
+                InvoiceDate = DateTime.UtcNow.ToShortDateString(),
+
+                CustName = "Maverick",
+                CustEmail = "maverick@yopmail.com",
+
+                    Items = new List<InvoiceItems>
+                    {
+                        new InvoiceItems
+                        {
+                            Desc = "Software Dev",
+                            Quantity = 1,
+                            Price = 5000
+                        },
+
+                        new InvoiceItems
+                        {
+                            Desc = "Cloud Dev",
+                            Quantity= 1,
+                            Price = 7000
+                        }
+                    }
+                },
+
+                new Invoice
+                {
+                    Id = 2,
+                InvoiceNumber = "INV - 0002",
+                InvoiceDate = DateTime.UtcNow.ToShortDateString(),
+
+                CustName = "John Doe",
+                CustEmail = "johnDoe@yopmail.com",
+
+                    Items = new List<InvoiceItems>
+                    {
+                        new InvoiceItems
+                        {
+                            Desc = "Widget A",
+                            Quantity = 1,
+                            Price = 19
+                        }
+                    }
+                }
+            };
+        }
+
+
+        public Invoice GetInvoicebyID(int id)
+        {
+            return _invoices.FirstOrDefault( i => i.Id == id);
+
+        }
+    }
+}
