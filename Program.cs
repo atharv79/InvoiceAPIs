0394c42580680d10a0518f4028717c5db83b59e9
Atharv Pawar <pawaratharv79@gmail.com>
2026-02-24 14:54:22 +0530

    API Project

--- Program.cs
+++ Program.cs
@@ -43,6 +43,9 @@ namespace InvoiceAPI
 
             app.MapControllers();
 
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+            app.Urls.Add($"http://*:{port}");
+
             app.Run();
         }
     }
